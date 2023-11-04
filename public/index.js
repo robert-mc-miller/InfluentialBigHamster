a046f0f260c9dbce004b25b92268321a1246d5a4
fn <64966099+f2reninj5@users.noreply.github.com>
2023-11-04 18:48:07 +0000

    Missing closing bracket

--- public/index.js
+++ public/index.js
@@ -1,10 +1,11 @@
 
-function openModal(id){
+function openModal(id) {
     document.getElementById(id).style.display = 'flex';
 }
 
-function closeModal(id){
+function closeModal(id) {
     document.getElementById(id).style.display = 'none';
+}
 
 const msInADay = 8.64e+7;
 const day1 = (new Date(0)).getDay();
@@ -26,14 +27,13 @@ $('document').ready(() => {                                                     
     })
 })
 
-function increaseDate(n = 1)
-{
+function increaseDate(n = 1) {
     let dateElement = document.getElementById("date");                                                  // Get HTML element of game date
     let upcomingElement = document.getElementById("upcoming");                                          // Get HTML element of upcoming expenses
     let date = new Date(game.date);                                                                     // Parse game date from JSON into date
     let newDate = new Date(date.getTime() + n * msInADay);                                              // Get the new date with added days
-    let day = newDate.getDate() < 10? `0${newDate.getDate()}` : `${newDate.getDate()}`;                 // Format date and month
-    let month = newDate.getMonth() < 10? `0${newDate.getMonth() + 1}` : `${newDate.getMonth() + 1}`;
+    let day = newDate.getDate() < 10 ? `0${newDate.getDate()}` : `${newDate.getDate()}`;                 // Format date and month
+    let month = newDate.getMonth() < 10 ? `0${newDate.getMonth() + 1}` : `${newDate.getMonth() + 1}`;
 
     game.date = newDate.getTime();                                                                      // Update JSON on client machine
 
@@ -48,12 +48,10 @@ function increaseDate(n = 1)
     nextRent -= n;                                                                                      // Decrement days until rent
     nextFood -= n;                                                                                      // Decrement days until food
 
-    while(nextRent < 0)
-    {
+    while (nextRent < 0) {
         nextRent += parseInt(new Date(1970, newDate.getMonth() + 1, 0).getDate());                      // Adjust new days left if going to a new month
     }
-    while(nextFood < 0)
-    {
+    while (nextFood < 0) {
         nextFood += 7
     }
 
