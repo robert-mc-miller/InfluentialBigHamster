84d6ad7d4179f23e9d29c600382a60a50f062d1a
Alessandro Cassani <a.cassani055@gmail.com>
2023-11-04 15:57:56 +0000

    Add comments to increaseDate() function

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 const msInADay = 8.64e+7;
-
+const day1 = Date(0).getDay();
 const game = 
 {
     'username': "",
@@ -20,14 +20,16 @@ $('document').ready(() => {
 
 function increaseDate(n = 1)
 {
-    let dateElement = document.getElementById("date");
-    let date = new Date(game.date);
-    let newDate = new Date(date.getTime() + n * msInADay);
-
-    let day = newDate.getDate() < 10? `0${newDate.getDate()}` : `${newDate.getDate()}`;
+    let dateElement = document.getElementById("date");                                                  // Get HTML element to update on page
+    let date = new Date(game.date);                                                                     // Parse game date from JSON into date
+    let newDate = new Date(date.getTime() + n * msInADay);                                              // Get the new date with added days
+    let day = newDate.getDate() < 10? `0${newDate.getDate()}` : `${newDate.getDate()}`;                 // Format date and month
     let month = newDate.getMonth() < 10? `0${newDate.getMonth() + 1}` : `${newDate.getMonth() + 1}`;
+
+    game.date = newDate.getTime();                                                                      // Update JSON on client machine
 
     console.log(newDate);
 
-    dateElement.innerHTML = `${day}/${month}`;
+    dateElement.innerHTML = `${day}/${month}`;                                                          //Update date on HTML
+
 }
