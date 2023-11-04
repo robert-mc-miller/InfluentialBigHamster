5d31a52e0f49639ad530e3936e35e834258515ab
fn <64966099+f2reninj5@users.noreply.github.com>
2023-11-04 18:43:00 +0000

    Added autosave

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(express.static('public'))
 
 app.post('/load', (req, res) => {
@@ -22,7 +22,7 @@ app.post('/load', (req, res) => {
 
 app.post('/save', (req, res) => {
     const game = req.body
-    const username = req.body.username
+    const username = game.username
 
     if (Object.keys(game).length === 0) {
         res.status(400)
--- public/index.js
+++ public/index.js
@@ -1,11 +1,43 @@
 
 let game = {}
+let saveInterval
+
+function getCookie(name) {
+    for (let cookie of document.cookie.split(';')) {
+        let parts = cookie.split('=')
+        if (parts[0] === name) {
+            return parts[1]
+        }
+    }
+    return undefined
+}
+
+$(window).on('load', () => {
+
+    let username = getCookie('username')
+
+    if (username) {
+        loadGame(username)
+    }
+
+    saveInterval = setInterval(() => {
+        saveGame()
+    }, 1000 * 60)
+})
+
+$(window).on('beforeunload', () => {
+    if (Object.keys(game).includes('username')) {
+        document.cookie = `username=${game.username}`
+    }
+    saveGame()
+})
 
 function loadGame(username) {
     $.ajax({
         method: 'post',
         url: '/load',
-        data: { username },
+        contentType: 'application/json',
+        data: JSON.stringify({ username }),
         success: (data) => {
             game = data
         }
@@ -17,8 +49,8 @@ function saveGame() {
         $.ajax({
             method: 'POST',
             url: '/save',
-            data: game,
-            contentType: 'application/json'
+            contentType: 'application/json',
+            data: JSON.stringify(game)
         })
     }
     else {
@@ -30,7 +62,8 @@ function createGame(username) {
     $.ajax({
         method: 'post',
         url: '/create',
-        data: { username },
+        contentType: 'application/json',
+        data: JSON.stringify({ username }),
         success: (data) => {
             game = data
         }
